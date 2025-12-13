573ad308c044bbe7f9bafa002e6b27c248fdc4d3
Luhsti <lucas_cavalcanti@id.uff.br>
2025-12-13 17:05:00 -0300

    finalização estrutura basica com css var

---
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -0,0 +1,30 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+type CardType = 'income' | 'outcome' | 'balance';
+type ValueCssClasses = 'income' | 'outcome'
+
+@Component({
+  selector: 'app-balance-card',
+  imports: [MatCardModule],
+  templateUrl: './balance-card.html',
+  styleUrl: './balance-card.scss',
+})
+export class BalanceCard {
+  type = input.required<CardType>(); 
+  label = input.required<string>();
+  value = input.required<number>();
+
+  cssClass = computed<ValueCssClasses>(() => {
+    if(this.type() === 'income'){
+      return 'income'
+    }
+
+    if (this.type() === 'outcome'){
+      return 'outcome'
+    }
+
+    return this.value() > 0 ? 'income' : 'outcome'
+  })
+
+}
